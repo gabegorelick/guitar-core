3b9f1f090ebd4fc4e817d156dc577614582b980f
ishanbanerjee <ishanbanerjee@cc9e4985-2604-4ca3-9a6b-1c23c61bd3f4>
2011-06-19 22:31:55 +0000

    Cosmetic cleanup only. No change in logic.
    git-svn-id: https://guitar.svn.sourceforge.net/svnroot/guitar/GUITARModel-Core/trunk@2645 cc9e4985-2604-4ca3-9a6b-1c23c61bd3f4

--- src/edu/umd/cs/guitar/util/Util.java
+++ src/edu/umd/cs/guitar/util/Util.java
@@ -1,21 +1,26 @@
-/*	
- *  Copyright (c) 2009-@year@. The GUITAR group at the University of Maryland. Names of owners of this group may
- *  be obtained by sending an e-mail to atif@cs.umd.edu
- * 
- *  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated 
- *  documentation files (the "Software"), to deal in the Software without restriction, including without 
- *  limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
- *	the Software, and to permit persons to whom the Software is furnished to do so, subject to the following 
- *	conditions:
- * 
- *	The above copyright notice and this permission notice shall be included in all copies or substantial 
- *	portions of the Software.
+/*
+ *  Copyright (c) 2009-@year@. The  GUITAR group  at the University of
+ *  Maryland. Names of owners of this group may be obtained by sending
+ *  an e-mail to atif@cs.umd.edu
  *
- *	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT 
- *	LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO 
- *	EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER 
- *	IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR 
- *	THE USE OR OTHER DEALINGS IN THE SOFTWARE. 
+ *  Permission is hereby granted, free of charge, to any person obtaining
+ *  a copy of this software and associated documentation files
+ *  (the "Software"), to deal in the Software without restriction,
+ *  including without limitation  the rights to use, copy, modify, merge,
+ *  publish,  distribute, sublicense, and/or sell copies of the Software,
+ *  and to  permit persons  to whom  the Software  is furnished to do so,
+ *  subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included
+ *  in all copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ *  OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ *  MERCHANTABILITY,  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ *  IN NO  EVENT SHALL THE  AUTHORS OR COPYRIGHT  HOLDERS BE LIABLE FOR ANY
+ *  CLAIM, DAMAGES OR  OTHER LIABILITY,  WHETHER IN AN  ACTION OF CONTRACT,
+ *  TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ *  SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 package edu.umd.cs.guitar.util;
 
@@ -44,87 +49,95 @@ import edu.umd.cs.guitar.model.GUITARConstants;
  */
 public class Util {
 
-    /**
-     * Hide the constructor
-     */
-    private Util() {
-    }
+   /**
+    * Hide the constructor
+    */
+   private Util()
+   {
+   }
 
-    /**
-     * Get a list of string from a text file. Used for ignoring widgets,
-     * windows...
-     * 
-     * @param sFileName
-     * @param isOrdered
-     * @return
-     */
+   /**
+    * Get a list of string from a text file. Used for ignoring widgets,
+    * windows.
+    * 
+    * @param sFileName
+    * @param isOrdered
+    * @return
+    */
+   public static List<String>
+   getListFromFile(String sFileName,
+                   boolean isOrdered)
+   {
+      List<String> retList;
 
-    public static List<String> getListFromFile(String sFileName,
-            boolean isOrdered) {
+      if (isOrdered) {
+           retList = new LinkedList<String>();
+      } else {
+         retList = new ArrayList<String>();
+      }
 
-        List<String> retList;
-
-        if (isOrdered) {
-            retList = new LinkedList<String>();
-        } else {
-            retList = new ArrayList<String>();
-        }
-
-        try {
+      try {
+         ClassLoader cl = Util.class.getClassLoader();
+         InputStream is = cl.getResourceAsStream(sFileName);
+         InputStreamReader isr = new InputStreamReader(is);
+         BufferedReader br = new BufferedReader(isr);
             
-            ClassLoader cl = Util.class.getClassLoader();
-            InputStream is = cl.getResourceAsStream(sFileName);
-            InputStreamReader isr = new InputStreamReader(is);
-            BufferedReader br = new BufferedReader(isr);
-            
-//            BufferedReader br = new BufferedReader(new FileReader(sFileName));
-
-            String line;
-            while ((line = br.readLine()) != null) {
-                line = line.trim();
-                if (!line.startsWith(GUITARConstants.IGNORE_COMMENT_PREFIX)
-                        && !("".equals(line)))
-                    retList.add(line);
+         String line;
+         while ((line = br.readLine()) != null) {
+            line = line.trim();
+            if (!line.startsWith(GUITARConstants.IGNORE_COMMENT_PREFIX)
+                && !("".equals(line))) {
+               retList.add(line);
             }
-        } catch (FileNotFoundException e) {
-			GUITARLog.log.error(e);
-        } catch (Exception e) {
-            GUITARLog.log.error( sFileName + " not found",e) ;
-        }
-        return retList;
-    }
+         }
+      } catch (FileNotFoundException e) {
+         GUITARLog.log.error(e);
+      } catch (Exception e) {
+         GUITARLog.log.error( sFileName + " not found",e) ;
+      }
 
-    @Deprecated
-    public static void getCollectionFromFile(String sFileName,
-            Collection<String> collection) {
-        try {
-            BufferedReader is = new BufferedReader(new FileReader(sFileName));
+      return retList;
+   }
 
-            String line;
-            while ((line = is.readLine()) != null) {
-                line = line.trim();
-                if (!line.startsWith(GUITARConstants.IGNORE_COMMENT_PREFIX)
-                        && !("".equals(line)))
-                    collection.add(line);
+   @Deprecated
+   public static void
+   getCollectionFromFile(String sFileName,
+                         Collection<String> collection)
+   {
+      try {
+         BufferedReader is = new BufferedReader(new FileReader(sFileName));
+
+         String line;
+         while ((line = is.readLine()) != null) {
+            line = line.trim();
+            if (!line.startsWith(GUITARConstants.IGNORE_COMMENT_PREFIX)
+                && !("".equals(line))) {
+               collection.add(line);
             }
-        } catch (FileNotFoundException e) {
-            // TODO Auto-generated catch block
-			GUITARLog.log.error(e);
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-			GUITARLog.log.error(e);
-        }
-    }
+         }
+      } catch (FileNotFoundException e) {
+         // TODO Auto-generated catch block
+         GUITARLog.log.error(e);
+      } catch (IOException e) {
+         // TODO Auto-generated catch block
+         GUITARLog.log.error(e);
+      }
+   }
 
-    /**
-     * get TimeStamp
-     * 
-     * @return
-     */
-    public static String getTimeStamp() {
-        DateFormat df = new SimpleDateFormat("yyyyMMddhhmmss");
-        df.setTimeZone(TimeZone.getTimeZone("GMT"));
-        return df.format(new Date());
-    }
 
-}
+   /**
+    * get TimeStamp
+    * 
+    * @return
+    */
+   public static String
+   getTimeStamp()
+   {
+      DateFormat df = new SimpleDateFormat("yyyyMMddhhmmss");
+
+      df.setTimeZone(TimeZone.getTimeZone("GMT"));
+
+      return df.format(new Date());
+   }
+
+} // Class
