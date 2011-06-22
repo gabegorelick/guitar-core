c22bd271526da43bfbc918f34f2167065b8b36a3
ishanbanerjee <ishanbanerjee@cc9e4985-2604-4ca3-9a6b-1c23c61bd3f4>
2011-06-22 06:39:36 +0000

    Cleanups only. No change in logic.
    git-svn-id: https://guitar.svn.sourceforge.net/svnroot/guitar/GUITARModel-Core/trunk@2654 cc9e4985-2604-4ca3-9a6b-1c23c61bd3f4

--- src/edu/umd/cs/guitar/model/wrapper/EFGWrapper.java
+++ src/edu/umd/cs/guitar/model/wrapper/EFGWrapper.java
@@ -1,21 +1,26 @@
-/*	
- *  Copyright (c) 2009-@year@. The GUITAR group at the University of Maryland. Names of owners of this group may
- *  be obtained by sending an e-mail to atif@cs.umd.edu
- * 
- *  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated 
- *  documentation files (the "Software"), to deal in the Software without restriction, including without 
- *  limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
- *	the Software, and to permit persons to whom the Software is furnished to do so, subject to the following 
- *	conditions:
- * 
- *	The above copyright notice and this permission notice shall be included in all copies or substantial 
- *	portions of the Software.
+/*
+ *  Copyright (c) 2009-@year@. The  GUITAR group  at the University of
+ *  Maryland. Names of owners of this group may be obtained by sending
+ *  an e-mail to atif@cs.umd.edu
  *
- *	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT 
- *	LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO 
- *	EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER 
- *	IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR 
- *	THE USE OR OTHER DEALINGS IN THE SOFTWARE. 
+ *  Permission is hereby granted, free of charge, to any person obtaining
+ *  a copy of this software and associated documentation files
+ *  (the "Software"), to deal in the Software without restriction,
+ *  including without limitation  the rights to use, copy, modify, merge,
+ *  publish,  distribute, sublicense, and/or sell copies of the Software,
+ *  and to  permit persons  to whom  the Software  is furnished to do so,
+ *  subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included
+ *  in all copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ *  OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ *  MERCHANTABILITY,  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ *  IN NO  EVENT SHALL THE  AUTHORS OR COPYRIGHT  HOLDERS BE LIABLE FOR ANY
+ *  CLAIM, DAMAGES OR  OTHER LIABILITY,  WHETHER IN AN  ACTION OF CONTRACT,
+ *  TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ *  SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 package edu.umd.cs.guitar.model.wrapper;
 
@@ -34,129 +39,131 @@ import edu.umd.cs.guitar.model.data.RowType;
 /**
  * @author <a href="mailto:baonn@cs.umd.edu"> Bao Nguyen </a>
  */
-public class EFGWrapper {
+public class EFGWrapper
+{
+   /**
+    * 
+    */
+   private static final String EVENT_NAME_PREFIX = "e";
+   ObjectFactory factory = new ObjectFactory();
 
-    /**
-     * 
-     */
-    private static final String EVENT_NAME_PREFIX = "e";
-    ObjectFactory factory = new ObjectFactory();
-    EFG efg;
-    EventsType dEventList;
+   EFG efg;
 
-    List<EventWrapper> wEventList;
+   EventsType dEventList;
 
-    // EventGraphType eventGraph;
-    List<List<String>> eventGraph;
+   List<EventWrapper> wEventList;
 
-    /**
-     * Generate efg graph (follow relations) from the GUI Structure
-     * 
-     * <p>
-     * 
-     * @param dGUIStructure
-     */
-    @Deprecated
-    public void parseEFG(GUIStructure dGUIStructure) {
-        GUIStructureWrapper wGUIStrcuture = new GUIStructureWrapper(
-                dGUIStructure);
-        wGUIStrcuture.parseData();
+   List<List<String>> eventGraph;
 
-        // Create event list
-        wEventList = new ArrayList<EventWrapper>();
+   /**
+    * Generate efg graph (follow relations) from the GUI Structure
+    * 
+    * <p>
+    * 
+    * @param dGUIStructure
+    */
+   @Deprecated
+   public void
+   parseEFG(GUIStructure dGUIStructure)
+   {
+      GUIStructureWrapper wGUIStrcuture =
+         new GUIStructureWrapper(dGUIStructure);
 
-        List<GUITypeWrapper> wWindowList = wGUIStrcuture.lGUI;
+      wGUIStrcuture.parseData();
 
-        for (GUITypeWrapper window : wWindowList) {
-            readEventList(window.container);
-        }
+      // Create event list
+      wEventList = new ArrayList<EventWrapper>();
 
-        efg = factory.createEFG();
+      List<GUITypeWrapper> wWindowList = wGUIStrcuture.lGUI;
 
-        // Reading event name
-        dEventList = factory.createEventsType();
+      for (GUITypeWrapper window : wWindowList) {
+         readEventList(window.container);
+      }
 
-        for (EventWrapper wEvent : wEventList) {
-            EventType dEvent = factory.createEventType();
-            dEvent.setWidgetId(wEvent.component
+      efg = factory.createEFG();
+
+      // Reading event name
+      dEventList = factory.createEventsType();
+
+      for (EventWrapper wEvent : wEventList) {
+         EventType dEvent = factory.createEventType();
+
+         dEvent.setWidgetId(wEvent.component
                     .getFirstValueByName(GUITARConstants.ID_TAG_NAME));
-            dEvent.setAction(wEvent.action);
+         dEvent.setAction(wEvent.action);
 
-            String eventType;
+         String eventType;
 
-//            // Change to match Charlie's code
-//            if (wEvent.component.window.isRoot() && !wEvent.isHidden())
-//                eventType = "r";
-//            else
-//                eventType = "e";
-//
-//            dEvent.setEventId(eventType + wEventList.indexOf(wEvent));
-            dEvent.setEventId(EVENT_NAME_PREFIX+ wEventList.indexOf(wEvent));
+         dEvent.setEventId(EVENT_NAME_PREFIX+ wEventList.indexOf(wEvent));
 
-            dEventList.getEvent().add(dEvent);
-        }
-        efg.setEvents(dEventList);
+         dEventList.getEvent().add(dEvent);
+      }
 
-        eventGraph = new ArrayList<List<String>>();
-        for (List<String> row : eventGraph) {
-            row = new ArrayList<String>();
-        }
-        //
-        EventGraphType dEventGraph = factory.createEventGraphType();
+      efg.setEvents(dEventList);
 
-        List<RowType> lRowList = new ArrayList<RowType>();
+      eventGraph = new ArrayList<List<String>>();
+      for (List<String> row : eventGraph) {
+         row = new ArrayList<String>();
+      }
 
-        for (EventWrapper firstEvent : wEventList) {
-            int indexFirst = wEventList.indexOf(firstEvent);
-            System.out.println("Anlyzing row: " + indexFirst);
-            RowType row = factory.createRowType();
+      EventGraphType dEventGraph = factory.createEventGraphType();
 
-            for (EventWrapper secondEvent : wEventList) {
-                int indexSecond = wEventList.indexOf(secondEvent);
-                int cellValue = firstEvent.isFollowedBy(secondEvent);
-                row.getE().add(indexSecond, cellValue);
-            }
-            lRowList.add(indexFirst, row);
-        }
-        dEventGraph.setRow(lRowList);
-        efg.setEventGraph(dEventGraph);
+      List<RowType> lRowList = new ArrayList<RowType>();
+
+      for (EventWrapper firstEvent : wEventList) {
+         int indexFirst = wEventList.indexOf(firstEvent);
+
+         System.out.println("Anlyzing row: " + indexFirst);
+         RowType row = factory.createRowType();
+
+         for (EventWrapper secondEvent : wEventList) {
+            int indexSecond = wEventList.indexOf(secondEvent);
+            int cellValue = firstEvent.isFollowedBy(secondEvent);
+            row.getE().add(indexSecond, cellValue);
+         }
+
+         lRowList.add(indexFirst, row);
+      }
+
+      dEventGraph.setRow(lRowList);
+      efg.setEventGraph(dEventGraph);
+   }
+
+
+   /**
+    * @return the efg
+    */
+   public EFG
+   getEfg() {
+      return efg;
+   }
+
+
+   /**
+    * @param window
+    */
+   private void
+   readEventList(ComponentTypeWrapper component)
+   {
+      List<String> sActionList = component
+         .getValueListByName(GUITARConstants.EVENT_TAG_NAME);
+
+      if (sActionList != null)
+         for (String action : sActionList) {
+            EventWrapper wEvent = new EventWrapper();
+            wEvent.action = action;
+            wEvent.component = component;
+            wEventList.add(wEvent);
+         }
+
+      List<ComponentTypeWrapper> wChildren = component.getChildren();
+
+      if (wChildren != null)
+         for (ComponentTypeWrapper wChild : wChildren) {
+            readEventList(wChild);
+         }
     }
 
-    /**
-     * @return the efg
-     */
-    public EFG getEfg() {
-        return efg;
-    }
-
-    /**
-     * @param window
-     */
-    private void readEventList(ComponentTypeWrapper component) {
-        // if (component.window != null) {
-        // System.out.println(component.window.getID()
-        // + " :"
-        // + component
-        // .getFirstValueByName(GUITARConstants.ID_TAG_NAME));
-        // }
-
-        List<String> sActionList = component
-                .getValueListByName(GUITARConstants.EVENT_TAG_NAME);
-
-        if (sActionList != null)
-            for (String action : sActionList) {
-                EventWrapper wEvent = new EventWrapper();
-                wEvent.action = action;
-                wEvent.component = component;
-                wEventList.add(wEvent);
-            }
-        List<ComponentTypeWrapper> wChildren = component.getChildren();
-
-        if (wChildren != null)
-            for (ComponentTypeWrapper wChild : wChildren) {
-                readEventList(wChild);
-            }
-    }
 
     /**
      * @return the wEventList
@@ -166,4 +173,4 @@ public class EFGWrapper {
     }
     
 
-}
+} // End of class
